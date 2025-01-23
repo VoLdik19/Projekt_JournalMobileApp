96c3aee3858a26a04eae321baec9bfc06cee9ca4
Jiží <j1_riha@utb.cz>
2025-01-23 19:29:55 +0100

    WORKING SPLASH SCREEN AND ICON FINALLY

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'Projekt',
+  appName: 'Journal',
   webDir: 'www',
   plugins: {
     SplashScreen: {
